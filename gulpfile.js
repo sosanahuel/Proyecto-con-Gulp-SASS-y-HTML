00de4144a8071f40b71778346119c1893e880d4d
Nahuel Sosa <xnahuex@gmail.com>
2023-02-13 17:46:58 -0300

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const {src, dest , watch, parallel } = require ("gulp");
+
+//css
+const sass = require("gulp-sass") (require ('sass'));
+const plumber = require('gulp-plumber');
+
+//imagenes
+const cache = require ('gulp-cache');
+const webp = require ('gulp-webp');
+const imagemin = require ('gulp-imagemin');
+const avif = require ('gulp-avif');
+ 
+
+function css(done) {
+    
+
+    src("./src/scss/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest("build/css"))
+
+    done();
+}
+
+function imagenes (done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('./src/img/**/*.{png,jpg}')
+        .pipe( cache(imagemin(opciones) ))
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionWebp( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('./src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionAvif( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('./src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
+function dev(done) {
+
+    watch("./src/scss/**/*.scss", css)
+
+    done();
+}
+
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel (imagenes, versionWebp, versionAvif, dev);
+exports.dev2 = dev;

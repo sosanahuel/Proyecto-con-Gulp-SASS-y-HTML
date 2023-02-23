41a6359641ee42b0a81f50550d215d830e10a4d0
Nahuel Sosa <xnahuex@gmail.com>
2023-02-23 20:48:50 -0300

    finalizando footer

--- gulpfile.js
+++ gulpfile.js
@@ -60,16 +60,24 @@ function versionAvif( done ) {
     done();
 }
 
-function dev(done) {
-
-    watch("./src/scss/**/*.scss", css)
+function javascript( done) {
+    src('./src/js/**/*.js')
+        .pipe(dest('build/js'));
 
     done();
 }
 
+function dev(done) {
+
+    watch("./src/scss/**/*.scss", css)
+    watch('./src/js/**/*.js', javascript);
+    done();
+}
+
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel (imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel (imagenes, versionWebp, versionAvif, javascript, dev);
 exports.dev2 = dev;
